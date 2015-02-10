7bc40a11affb6745accf4c084e9c553ed22cc0d4
James Gordon <gordo@mwc-178029.dhcp.missouri.edu>
2015-02-10 13:16:36 -0600

    Setting appropriate data_types.

---
+++ create_social_flow.sql
@@ -0,0 +1,16 @@
+CREATE TABLE social_flow (
+          Account_Name VARCHAR(255)
+        , Account_Type VARCHAR(255)
+        , Created_By VARCHAR(255)
+        , Publish_Date DATE
+        , Time_UTC TIME
+        , Message VARCHAR(8000)
+        , Publish_Status VARCHAR(255)
+        , Labels VARCHAR(255)
+        , Clicks BIGINT
+        , Retweets_Shares BIGINT
+        , Likes_Favorites_plus1s BIGINT
+        , Comments BIGINT
+        , Reach VARCHAR(255)
+        , Link VARCHAR(255)
+);
---
+++ load_social_flow.sql
@@ -0,0 +1,10 @@
+COPY social_flow
+FROM '/Users/gordo/missourian_analytics/data/Feb12to15_feb_09.csv'
+WITH CSV HEADER NULL '';
+
+UPDATE social_flow
+SET Reach = NULL
+WHERE Reach = 'None';
+
+ALTER TABLE social_flow ALTER COLUMN Reach TYPE BIGINT USING CAST(Reach AS INTEGER);
+
