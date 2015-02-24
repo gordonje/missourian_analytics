7710da830b6bac688be14b41e064c78eef77d7b4
James Gordon <gordo@Jamess-MacBook-Pro.local>
2015-02-23 20:02:07 -0600

    Outputting relational tables

--- export_social_flow.sql
+++ export_social_flow.sql
@@ -2,3 +2,11 @@
 COPY social_flow
 TO '/Users/gordo/missourian_analytics/data/Feb12to15_feb_09_appd.csv'
 WITH CSV HEADER NULL '';
+
+COPY hash_tags
+TO '/Users/gordo/missourian_analytics/data/social_flow_hash_tags.csv'
+WITH CSV HEADER NULL '';
+
+COPY at_tags
+TO '/Users/gordo/missourian_analytics/data/social_flow_at_tags.csv'
+WITH CSV HEADER NULL '';
