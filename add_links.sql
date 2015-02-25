927a2a403ec537dae28dcbfa2e97a1839ae72fb4
James Gordon <gordo@vdi-058124.dhcp.missouri.edu>
2015-02-25 17:17:59 -0600

    Added links relational table and count column.

---
+++ add_links.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE message_links AS
+SELECT id as message_id, regexp_matches(message, 'http://[\w./-]+', 'g') as link
+FROM social_flow;
+
+ALTER TABLE social_flow
+ADD COLUMN link_count INT DEFAULT 0;
+
+UPDATE social_flow
+SET link_count = the_count
+FROM (
+        SELECT message_id, COUNT(*) as the_count
+        FROM message_links
+        GROUP BY 1
+) as foo
+WHERE id = message_id; 
