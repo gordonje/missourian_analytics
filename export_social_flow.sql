40bd5c2d25709e5f572cb79ecf4c5320f38d0206
James Gordon <gordo@Jamess-MacBook-Pro.local>
2015-02-25 19:26:47 -0600

    Added links and fixed table names.

--- export_social_flow.sql
+++ export_social_flow.sql
@@ -3,10 +3,14 @@ COPY social_flow
 TO '/Users/gordo/missourian_analytics/data/Feb12to15_feb_09_appd.csv'
 WITH CSV HEADER NULL '';
 
-COPY hash_tags
-TO '/Users/gordo/missourian_analytics/data/social_flow_hash_tags.csv'
+COPY messages_hashtags
+TO '/Users/gordo/missourian_analytics/data/social_flow_hashtags.csv'
 WITH CSV HEADER NULL '';
 
-COPY handles
-TO '/Users/gordo/missourian_analytics/data/social_flow_at_tags.csv'
+COPY messages_handles
+TO '/Users/gordo/missourian_analytics/data/social_flow_handles.csv'
+WITH CSV HEADER NULL '';
+
+COPY messages_links
+TO '/Users/gordo/missourian_analytics/data/social_flow_links.csv'
 WITH CSV HEADER NULL '';
