08577b008285788e497545a17870d82d7ccc037c
gordon.je@gmail.com <gordo@Jamess-MacBook-Pro.local>
2015-03-25 11:06:17 -0500

    Increased char_length of full_url

--- sql/create_short_to_full_urls.sql
+++ sql/create_short_to_full_urls.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE IF NOT EXISTS short_to_full_urls (
           short_url VARCHAR(255)
-        , full_url VARCHAR(255)
+        , full_url VARCHAR(400)
         , scheme  VARCHAR(255)
         , netloc VARCHAR(255)
         , url_path VARCHAR(255)
