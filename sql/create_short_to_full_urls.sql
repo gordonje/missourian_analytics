213e07235acd1cbf6351689d21fccededbd577f1
gordon.je@gmail.com <gordo@Jamess-MBP.PK5001Z>
2015-03-26 11:03:41 -0500

    Increasing char length of url fields, fixing bugs in select query.

--- sql/create_short_to_full_urls.sql
+++ sql/create_short_to_full_urls.sql
@@ -5,8 +5,9 @@ CREATE TABLE IF NOT EXISTS short_to_full_urls (
         , full_url VARCHAR(400)
         , scheme  VARCHAR(255)
         , netloc VARCHAR(255)
-        , url_path VARCHAR(255)
-        , params VARCHAR(255)
-        , query VARCHAR(255)
-        , fragment VARCHAR(255)
+        , url_path VARCHAR(400)
+        , params VARCHAR(400)
+        , query VARCHAR(400)
+        , fragment VARCHAR(400)
 );
+
--- sql/select_short_urls.sql
+++ sql/select_short_urls.sql
@@ -7,7 +7,6 @@ WHERE (
         OR link LIKE '%trib.al%'
         OR link LIKE '%tribal%'
         OR link LIKE '%ow.ly%'
-        OR link LIKE '%owly%'
         OR link LIKE '%t.co%'
 )
 AND link NOT IN (SELECT short_url FROM short_to_full_urls)
